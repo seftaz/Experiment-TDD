d8dba2fb97b729552af738e68e99b36bd32ea417
AliMarshmallow <79265188+AliMarshmallow@users.noreply.github.com>
2024-03-11 19:15:43 +0330

    Make searchBooks pass notName test

--- src/main/classes/Library.java
+++ src/main/classes/Library.java
@@ -93,8 +93,10 @@ public class Library {
      * @return The list of books that match the search criteria. Returns null if search type is name.
      */
     public ArrayList<Book> searchBooks(SearchByType searchByType, ArrayList<Object> keys) {
-        // TODO complete function
-        return null;
+        if (searchByType == SearchByType.NAME) {
+            return null;
+        }
+        return books;
     }
 
     /**
