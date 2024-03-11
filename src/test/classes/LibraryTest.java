dbafa35f4b260c128ca072f82de919dfedd9be08
AliMarshmallow <79265188+AliMarshmallow@users.noreply.github.com>
2024-03-11 18:31:23 +0330

    Add test into test.classes

---
+++ src/test/classes/LibraryTest.java
@@ -0,0 +1,14 @@
+package test.classes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+
+    @org.junit.jupiter.api.Test
+    void lendBook() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void returnBook() {
+    }
+}
